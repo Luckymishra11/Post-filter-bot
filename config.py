74dd2dff79e9a970229fdc384d2e33242d234049
Luckymishra11 <126936878+Luckymishra11@users.noreply.github.com>
2023-06-23 13:50:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", "8321183"))
-  API_HASH = os.environ.get("API_HASH", "d9102799310e7038de04d9af2679ed68")
-  BOT_TOKEN = os.getenv("BOT_TOKEN", "6124714988:AAHf0FZOHmagJ5W_XU5sIgvsgrPxBGqVHFo")
-  STRINGSESSION = os.environ.get("STRINGSESSION", "1BVtsOHcBu5sUSNrqSFZ8v_GNQRTP1t6MV4qaSW2slMBC8wgr7CGJLbE9weLEnSXVp6Dm2VUfE9OZSyhs6ScRDzo4XVkpWjz0YXMPEFKTpSNshB7s7szPkuCmb3QeIjUTc-0eulxOEk__DVcaGC4zsYXY1O1XPz2A2VIKNvffaPSeJh3KqKFl6noevVkItuBCiIswdxJH0q-zVo1BXiEVz_Md0OnGODJcT9Ot0V8QdgTQ-mBwv0X4e-KSkmK0dO2qVgXoHXzb84tlU1Cs_MAhzKuBvM9fXnsReCI3jmNPkbiYQe1ZR8aFOzI_iA_O3-41u_9itDOHY--6bmoBSYOGENRPbr7lmnY=")
+  API_ID = int(os.environ.get("API_ID", "20862286"))
+  API_HASH = os.environ.get("API_HASH", "b069c4c5a46d755502e2a21f278b40ee")
+  BOT_TOKEN = os.getenv("BOT_TOKEN", "6195387133:AAFFfYbDM97clW4f99j-b2ObmDKDHJpgj5c")
+  STRINGSESSION = os.environ.get("STRINGSESSION", "1BVtsOL0BuwXpZAJ7_XnMxiF3fF3RHQTjrfZSlaqpZ-BB2HGNfCpm1wWwTz9-7iZbgTblELfDRYdmXA8Rb5g6k729CgVuK3u5gJzlco3G1zR27SAIHkPFVmB4OtOPmYBioQ9scEoCoPO8sHi123vZOfsuJSToh3kGFE9e3l8gKolLyWKxSqj5yx7umELPFLFoauAY8icQqEGWS0WKDGc-fip55--zGYl_YoDqOjropiKm5-Y3KRsjEGTJBD7wWyasz60SfZFaUAxH4NU-pFvTK1G_SajEpklsSzb1eLbR9DqmblXHR3ezNGYiajr3e0eApYViN2BRiTDzSgz6N4-czvTM7-C69k8=")
   OWNER_ID = int(os.environ.get("OWNER_ID", "5651594253"))
   DATABASE_CHANNELS = os.environ.get("DATABASE_CHANNELS", ["-1001932233757"])
   DATABASE_URL = os.environ.get("DATABASE_URL", "postgres://kxygsjfk:KO8VSr9tAOXjo3rbmu8wMG9pBcN3veZb@babar.db.elephantsql.com/kxygsjfk")
